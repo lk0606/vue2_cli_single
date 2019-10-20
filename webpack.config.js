872da597cf42fc43b3f69835c74a853dd5ee37af
lk <919590347@qq.com>
2019-10-20 23:26:22 +0800

    '简单热更新配置完成，但是效果未实现，原因未知’

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
     watch: false,
     watchOptions: {
@@ -10,17 +12,24 @@ module.exports = {
         aggregateTimeout: 300,
         // 轮询是否发生变化 默认每秒1000次 也就是1ms/次
         poll: 1000,
-
     },
     entry: {
         index: './src/index.js',
         search: './src/search.js'
     },
+    /**
+     * 文件指纹 [name][(hash|chunkhash|contenthash)].[ext]
+     * name entry name
+     * hash 一个文件修改，则整个项目构建的hash值也将更改
+     * chunkhash 和webpack打包的chunk有关，根据entry生成不同chunkhash
+     * contenthash 根据文件内容生成hash，内容不变，则contenthash不变
+     * ext 文件后缀
+     */
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: '[name]_[chunkhash:4].js'
+		filename: '[name][chunkhash].js'
 	},
-	mode: 'production',
+	mode: 'development',
     module: {
 	    rules: [
             {
@@ -45,7 +54,7 @@ module.exports = {
             // 以下使用 url-loader替换file-loader
             // 原因： url-loader基于file-loader 多了小字体自动转base64 limit来实现
             {
-                test: /.(png|jpe?g|gif|svg)$/,
+                test: /.(png|jpe?g|gif|svg)(\?.*)?$/,
                 use: {
                     loader: 'url-loader',
                     options: {
@@ -54,9 +63,17 @@ module.exports = {
                 },
             },
             {
-                test: /.(woff2?|eot|ttf|otf)$/,
+                test: /.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 use: 'file-loader',
             },
         ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        // open: false,
+        contentBase: './dist',
+        hot: true
     }
 }
