b424798c150396e110fe5efa8a6c8c2698f5efea
lk <919590347@qq.com>
2019-10-20 21:36:55 +0800

    '准备学习热更新'

---
+++ src/search.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.less';
+import logo from './img/logo.png'
+
+class Search extends React.Component {
+    render() {
+        return <div className="search-text">
+            <span>Search Text</span>
+            <img src={logo} />
+        </div>;
+    }
+}
+
+ReactDOM.render (
+    <Search />,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,23 @@
 
 const path = require('path');
 module.exports = {
-	entry: './src/index.js',
+    watch: false,
+    watchOptions: {
+        // 默认为空 支持正则，不监听
+        ignored: /node_modules/,
+        // aggregateTimeout ms 后执行  默认300ms
+        aggregateTimeout: 300,
+        // 轮询是否发生变化 默认每秒1000次 也就是1ms/次
+        poll: 1000,
+
+    },
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js'
+    },
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: '[name]_[chunkhash:4].js'
 	},
 	mode: 'production',
     module: {
@@ -13,7 +26,37 @@ module.exports = {
             {
                 test: /.js$/,
                 use: 'babel-loader'
-            }
+            },
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+            // 以下使用 url-loader替换file-loader
+            // 原因： url-loader基于file-loader 多了小字体自动转base64 limit来实现
+            {
+                test: /.(png|jpe?g|gif|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10240
+                    }
+                },
+            },
+            {
+                test: /.(woff2?|eot|ttf|otf)$/,
+                use: 'file-loader',
+            },
         ]
     }
 }
