8773a8f57244bf226b37eee52624e8355bca5a4e
liukun <919590347@qq.com>
2020-01-22 18:51:28 +0800

    dll

---
+++ build/webpack.dll.js
@@ -0,0 +1,24 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        library: [
+            'react',
+            'react-dom'
+        ]
+    },
+    output: {
+        filename: '[name]_[hash].dll.js',
+        path: path.join(__dirname, './library'),
+        library: '[name]'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            name: '[name]_[hash]',
+            path: path.join(__dirname, './library/[name].json')
+        })
+    ]
+};
